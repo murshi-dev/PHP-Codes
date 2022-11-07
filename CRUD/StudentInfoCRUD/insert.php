a2743df0e687963604ea8edf66e1d315fa58c3d1
murshi <90964789+murshi-dev@users.noreply.github.com>
2022-11-07 17:12:06 +0800

    c
    c

---
+++ CRUD/StudentInfoCRUD/insert.php
@@ -0,0 +1,65 @@
+<?php
+//connection codes
+//create a function and write all the connection codes here
+function pdo_connect_mysql()
+{
+    try//holds the normal set of codes here(if no error occurs)
+    {
+        return new PDO('mysql:host=localhost; dbname=StudentInfo','root','');
+    }
+    catch(PDOException $exception)//in case error happens, 
+    //what to display/do-write it here
+    {
+        exit('could not connect to database');
+    }
+
+}
+//execute the function above and put in a local variable $pdo
+$pdo=pdo_connect_mysql();
+//insert query process starts here
+//check if form has been submitted
+if(!empty($_POST))
+{
+    //$id- to auto increment 
+    $id=isset($_POST['id']) && ($_POST['id'] != 'auto' ? $_POST['id'] : NULL);
+    //retrieve all the entries from each text box and store in a local variable
+    $studentname=$_POST['studentname'];
+    $contact=$_POST['contact'];
+    $address=$_POST['address'];
+    $state=$_POST['state'];
+    $country=$_POST['country'];
+    $postcode=$_POST['postcode'];
+    $yearintake=$_POST['yearintake'];
+
+    //all text box values retieved now
+    //now use the prepare () and execute()
+    //? --here refers to the run time values obtained throught the text boxes
+    $stmt=$pdo->prepare('INSERT INTO students values (?,?,?,?,?,?,?,?)');
+    $stmt->execute([$id,$studentname,$contact,$address,$state,$country,$postcode,$yearintake]);
+
+
+}
+?>
+<html>
+    <body>
+        <h2>Insert Records to the MySQL table</h2>
+        <!-- design the form for the user to enter input -->
+        <form action="insert.php" method="POST">
+            <label>Student Name</label>
+            <input type="text" name="studentname"><br>
+            <label>Contact</label>
+            <input type="text" name="contact"><br>
+            <label>Address</label>
+            <input type="text" name="address"><br>
+            <label>State</label>
+            <input type="text" name="state"><br>
+            <label>Country</label>
+            <input type="text" name="country"><br>
+            <label>Post Code</label>
+            <input type="text" name="postcode"><br>
+            <label>Year Intake</label>
+            <input type="text" name="yearintake"><br>
+            <input type="submit" name="Save Student Data">
+        </form>
+    </body>
+</html>
---
+++ CRUD/StudentInfoCRUD/view.php
@@ -0,0 +1,61 @@
+<?php
+//create a function and write all the connection codes here
+function pdo_connect_mysql()
+{
+    try//holds the normal set of codes here(if no error occurs)
+    {
+        return new PDO('mysql:host=localhost; dbname=StudentInfo','root','');
+    }
+    catch(PDOException $exception)//in case error happens, 
+    //what to display/do-write it here
+    {
+        exit('could not connect to database');
+    }
+
+}
+//execute the function above and put in a local variable $pdo
+$pdo=pdo_connect_mysql();
+
+//query the Database
+//use prepare statement to write the query
+$stmt=$pdo->prepare('SELECT * FROM students');
+//use execute() to execute the query
+$stmt->execute();
+//table has more than on erecord-hence
+//use fetchAll() to retrieve records one by one and store in a variable
+$posts=$stmt->fetchAll(PDO::FETCH_ASSOC);//FETCH ASSOC refers to associative array
+?>
+<html>
+    <body>
+        <h2>Read Data From Students Table</h2>
+        <!-- design a table structure to display the contents of students table -->
+        <table>
+            <thead>
+                <tr>ID</tr>
+                <tr>Student Name</tr>
+                <tr>Contact</tr>
+                <tr>Address</tr>
+                <tr>State</tr>
+                <tr>Postcode</tr>
+                <tr>Country</tr>
+                <tr>Year Intake</tr>
+            </thead>
+            <tbody>
+                <!-- use for each loop to display the contents of '$posts'
+                     array variable-->
+                <?php foreach ($posts as $post):?>
+                    <tr>
+                        <!-- fill each column of the row with relevant column -->                        <td><?=$post['id']?></td>  
+                        <td><?=$post['studentname']?></td>
+                      <td>  <?=$post['contact']?></td>
+                      <td>  <?=$post['address']?></td>
+                      <td>  <?=$post['state']?></td>
+                      <td>  <?=$post['postcode']?></td>
+                      <td>  <?=$post['country']?></td>
+                      <td>  <?=$post['yearintake']?></td>
+                    </tr>
+                    <?php endforeach;?>
+            </tbody>
+        </table>
+    </body>
+</html>
